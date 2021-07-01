6bb241213459c6cbe1c3703f1b6edc6cfad171dd
iampreviero <previero@mit.edu>
2021-07-01 13:00:41 -0400

    fixed header

--- frontend/dashboard/src/utilities/header.js
+++ frontend/dashboard/src/utilities/header.js
@@ -1,7 +1,7 @@
 function Header(props) {
     return (
-        <div className="flex flex-col justify-center h-auto p-4 w-screen bg-gray-700">
-                <span className="font-inter text-6xl text-gray-50 font-normal text-center">
+        <div className="flex flex-col justify-center h-auto p-2 w-screen bg-gray-700">
+                <span className="font-inter text-6xl text-gray-50 font-light text-center pb-1">
                     {props.title}
                 </span>
             <span className="font-inter text-2xl text-gray-300 font-extralight text-center">
