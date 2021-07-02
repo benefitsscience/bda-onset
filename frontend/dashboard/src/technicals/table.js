0185cac96d42718990c92e35ae1cc9bb02274340
iampreviero <previero@mit.edu>
2021-07-02 11:12:30 -0400

    Table typography

--- frontend/dashboard/src/technicals/table.js
+++ frontend/dashboard/src/technicals/table.js
@@ -1,19 +1,37 @@
+import "tailwindcss-font-inter";
 import { DataGrid } from '@material-ui/data-grid';
-import {makeStyles } from '@material-ui/core/styles';
+import {makeStyles, createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles';
 
 function Table(props){
+
+    // `body2` corresponds to Table text-body.
+    const THEME = createMuiTheme({
+      typography: {
+        "fontFamily": "inter",
+        "fontSize": 14,
+        useNextVariants: true,
+        suppressDeprecationWarnings: true,
+        body2: {
+            "fontWeight": 300,
+        },
+        subtitle2: {
+            "fontWeight": 700,
+        }
+      },
+    });
 
     const tableWidth = (150 * props.columns.length) + 10
     const useStyles = makeStyles({
         dataGrid: {width: tableWidth.toString().concat("px"),
-
         }
-    });
+    }
+    );
     const classes = useStyles();
 
     return(
         <div className="flex overflow-auto">
             <div className="h-96 w-auto max-w-7xl">
+                <MuiThemeProvider theme={THEME}>
                 <DataGrid
                     rows={props.rows}
                     columns={props.columns}
@@ -21,6 +39,7 @@ function Table(props){
                     checkboxSelection
                     className={classes.dataGrid}
                 />
+                </MuiThemeProvider>
             </div>
         </div>
     )
