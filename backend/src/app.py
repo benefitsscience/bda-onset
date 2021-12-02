172f4c6469ca16110adec3ef015ceea83531045a
iampreviero <alessandro.previero@previero.eu>
2021-12-01 22:40:12 -0500

    Refactor project structure

--- backend/src/app.py
+++ backend/src/app.py
@@ -2,15 +2,11 @@
 Main flask interface
 """
 from flask import Flask, request
-from run import run
 
 app = Flask(__name__)
 
 
-@app.route('/regression', methods=['POST'])
-def run_regression():
-
+@app.route('/data', methods=['POST'])
+def get_data():
     data = request.get_json()
-
-    weights = list(run(l2_weight=data['data']))
-    return {'coefficients': weights}
+    return 0
